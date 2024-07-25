917ca00ff7446d6c9d860b970251545682b24017
guilherme <guimedeiroxdev@gmail.com>
2024-07-25 15:27:51 -0300

    having fun with lambda

---
+++ lambdaExpressionchallenge/lambdaExpressionChallenge.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+class lambdaExpressionChallenge{ 
+
+    public static void main(String[] args) {
+        
+
+        List<String> list = new ArrayList<>();
+        list.add("Teste");
+        list.add("Teste");
+        list.add("Teste");
+        list.add("Teste");
+
+    }
+    
+}
